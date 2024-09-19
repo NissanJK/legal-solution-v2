315977ea80efb19a3c7ec1331044996642cab3db
NissanJK <jawadul.karim78@gmail.com>
2024-09-19 22:19:33 +0600

    trial fixing

--- js/login.js
+++ js/login.js
@@ -6,10 +6,10 @@ document.getElementById('Login-btn')
         const loginPass = document.getElementById('Login-pass').value;
 
         if((loginName === 'user1' && loginPass === '1111')||(loginName === 'user2' && loginPass === '2222')||(loginName === 'user3' && loginPass === '3333')||(loginName === 'user4' && loginPass === '4444')){
-            window.location.href = '/home_page.html';
+            window.location.href = './home_page.html';
         }
         else if(loginName === 'admin' && loginPass === '1234'){
-            window.location.href = '/admin_page.html';
+            window.location.href = './admin_page.html';
         }
         else{
             alert('Wrong Name or Password.')
