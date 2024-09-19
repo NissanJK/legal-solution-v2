fd9136be74215beaecc396274bb238a58f2e17bc
NissanJK <jawadul.karim78@gmail.com>
2024-09-19 22:11:51 +0600

    updated to v2 semi finished

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('Login-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const loginName = document.getElementById('Login-name').value;
+        const loginPass = document.getElementById('Login-pass').value;
+
+        if((loginName === 'user1' && loginPass === '1111')||(loginName === 'user2' && loginPass === '2222')||(loginName === 'user3' && loginPass === '3333')||(loginName === 'user4' && loginPass === '4444')){
+            window.location.href = '/home_page.html';
+        }
+        else if(loginName === 'admin' && loginPass === '1234'){
+            window.location.href = '/admin_page.html';
+        }
+        else{
+            alert('Wrong Name or Password.')
+        }
+});
